91ace1f2b6f9fdf0a72f9b233aa5da883297a09c
Gopala <78594040+gopala4813@users.noreply.github.com>
2021-11-26 13:54:16 +0530

    Create object.js

---
+++ object.js
@@ -0,0 +1,21 @@
+var person1=
+{
+    firstname:"GOPALA",
+    lastname:"PATEL",
+    officeAddress:"BEDIA , DIST - KHARGONE , (M.P.)"
+};
+var person2=
+{
+    firstname:"SAWAN",
+    lastname:"PATIDAR",
+    officeAddress:"KHARGONE , (M.P.)"
+};
+var person3=
+{
+    firstname:"PAWAN",
+    lastname:"PATEL",
+    officeAddress:"BARWAH , DIST - KHARGONE , (M.P)"
+};
+console.log(person1);
+console.log(person2);
+console.log(person3);
